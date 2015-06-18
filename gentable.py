8d577bf1ed905585be846471c5244fcc27dc34da
mfesyuk <lalm85@butovo.com>
2015-06-18 08:58:51 +0300

    Update gentable.py

--- gentable.py
+++ gentable.py
@@ -2,23 +2,29 @@
 # -*- coding: utf-8 -*-
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
+# get quantity of students, tasks and number of group
+tasks = int(input())
+students = int(input())
+number = int(input())
+length = 180 + 75 * tasks
+height = 20 * (students + 1)
+
 # make table
-img = Image.new('RGB', (820, 260), (255, 255, 255))
+img = Image.new('RGB', (length + 40, height + 40), (255, 255, 255))
 imgDrawer = ImageDraw.Draw(img)
-imgDrawer.rectangle((20, 20, 800, 240), fill = "#FFFFFF", outline = "#000000")
+imgDrawer.rectangle((20, 20, length + 20, height + 20), fill = "#FFFFFF", outline = "#000000")
 imgDrawer.rectangle((0, 0, 20, 20), fill = "#000000", outline = "#000000")
-imgDrawer.rectangle((800, 0, 820, 20), fill = "#000000", outline = "#000000")
-imgDrawer.rectangle((0, 240, 20, 260), fill = "#000000", outline = "#000000")
-imgDrawer.rectangle((800, 240, 820, 260), fill = "#000000", outline = "#000000")
-for i in range(200, 800, 75):
-	imgDrawer.line((i, 20, i, 240), fill = "#000000", width = 1)
-for i in range(40, 240, 20):
-	imgDrawer.line((20, i, 800, i), fill = "#000000", width = 1)
-for i in range(8):
+imgDrawer.rectangle((length + 20, 0, length + 40, 20), fill = "#000000", outline = "#000000")
+imgDrawer.rectangle((0, height + 20, 20, height + 40), fill = "#000000", outline = "#000000")
+imgDrawer.rectangle((length + 20, height + 20, length + 40, height + 40), fill = "#000000", outline = "#000000")
+for i in range(200, length + 20, 75):
+	imgDrawer.line((i, 20, i, length + 20), fill = "#000000", width = 1)
+for i in range(40, height + 20, 20):
+	imgDrawer.line((20, i, length + 20, i), fill = "#000000", width = 1)
+for i in range(tasks):
 	imgDrawer.text((200 + 75 * i + 37, 25), str(i + 1), fill = "black")
 
 # draw lines for number of group
-number = int(input())
 for i in range(number):
 	imgDrawer.line((20 + 180 // (number + 1) * (i + 1), 20, 20 + 180 // (number + 1) * (i + 1), 40), fill = "#000000", width = 1)
 
@@ -26,11 +32,9 @@ for i in range(number):
 surnames = []
 with open('groups.txt', 'r') as f_read:
 	for line in f_read:
-		line.decode('cp1251')
-		line = unicode(line, 'cp866')
-		surnames.append(line[:-1])
-for i in range(10):
-	imgDrawer.text((30, 20 * (i + 2) + 5), surnames[11 * (number - 1) + i + 1], font = ImageFont.truetype("DejaVuSans.ttf", 12), fill = (0,0,0))
+		surnames.append(line.decode(encoding = "utf8")[:-1])
+for i in range(students):
+	imgDrawer.text((30, 20 * (i + 2) + 5), surnames[(students + 1) * (number - 1) + i + 1], font = ImageFont.truetype("DejaVuSans.ttf", 12), fill = (0,0,0))
 
 # save image
 img.save("pil-example.png")

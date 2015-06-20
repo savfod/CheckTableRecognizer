ce59c3a73b6683bd00ef56c7d5e4efa741d8c26a
mfesyuk <lalm85@butovo.com>
2015-06-20 21:34:24 +0300

    Update gentable.py

--- gentable.py
+++ gentable.py
@@ -3,9 +3,16 @@
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
 
 # get quantity of students, tasks and number of group
-tasks = int(input())
-students = int(input())
-number = int(input())
+surnames = []
+with open('group.txt', 'r') as f_read:
+	for line in f_read:
+		surnames.append(line.decode(encoding = "utf8"))
+for i in range(len(surnames)):
+	if i < len(surnames) - 1:
+		surnames[i] = surnames[i][:-1]
+group = int(surnames[0])
+tasks = int(surnames[1])
+students = len(surnames) - 2
 length = 180 + 75 * tasks
 height = 20 * (students + 1)
 
@@ -25,16 +32,12 @@ for i in range(tasks):
 	imgDrawer.text((200 + 75 * i + 37, 25), str(i + 1), fill = "black")
 
 # draw lines for number of group
-for i in range(number):
-	imgDrawer.line((20 + 180 // (number + 1) * (i + 1), 20, 20 + 180 // (number + 1) * (i + 1), 40), fill = "#000000", width = 1)
+for i in range(group):
+	imgDrawer.line((20 + 180 // (group + 1) * (i + 1), 20, 20 + 180 // (group + 1) * (i + 1), 40), fill = "#000000", width = 1)
 
 # write names
-surnames = []
-with open('groups.txt', 'r') as f_read:
-	for line in f_read:
-		surnames.append(line.decode(encoding = "utf8")[:-1])
 for i in range(students):
-	imgDrawer.text((30, 20 * (i + 2) + 5), surnames[(students + 1) * (number - 1) + i + 1], font = ImageFont.truetype("DejaVuSans.ttf", 12), fill = (0,0,0))
+	imgDrawer.text((30, 20 * (i + 2) + 5), surnames[i + 2], font = ImageFont.truetype("DejaVuSans.ttf", 12), fill = (0,0,0))
 
 # save image
-img.save("pil-example.png")
+img.save("new-conduit.png")
